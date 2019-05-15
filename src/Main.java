b6a1dc603946957302d093b853df6ba1cdfad2f0
andreypalman <palmana@post.bgu.ac.il>
2019-05-15 10:54:31 +0300

    all the basic classes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,54 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.Collection;
+
 public class Main {
+
     public static void main(String[] args){
 
+        Player SelectedPlayer;
+        LinkedList<Player> players;
+
+        players = playersInit();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Select player:");
+        int selectedPlayer = -1;
+
+        selectedPlayer = scanner.nextInt();
+
+        switch (selectedPlayer){
+            case 1:{
+
+                break;
+            }
+            case 2:{
+
+                break;
+            }
+            case 3:{
+
+                break;
+            }
+            case 4:{
+
+                break;
+            }
+            case 5:{
+
+
+                break;
+            }
+            case 6:{
+
+                break;
+            }
+        }
+
+    }
+
+    private static LinkedList<Player> playersInit(){
+        LinkedList<Player> players = new LinkedList<>();
+        return players;
     }
 }
