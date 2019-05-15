b704693fd31e51d27089d8ad22d4abad42787fa8
hadarkorny@gmail.com <hadarkorny@gmail.com>
2019-05-15 10:51:45 +0300

    all the basic classes

--- src/Warrior.java
+++ src/Warrior.java
@@ -1,2 +1,32 @@
-public class Warrior {
+public class Warrior extends Player {
+
+
+    private int cooldown;
+    private int remaining;
+
+    public Warrior(int cooldown, Integer experience, Integer level, SpecialAbility specialAbility, String name, Health health, Integer attackPoints, Integer defencePoints, Position position){
+        super(experience, level, specialAbility,name,health, attackPoints,defencePoints, position);
+        this.remaining=0;
+        this.cooldown = cooldown;
+    }
+
+    @Override
+    public boolean play() {
+        return false;
+    }
+
+    public void gametick(){
+        remaining= remaining-1;
+    }
+
+    public boolean cast(){
+        if (remaining > 0){
+            return false;
+        }
+        else {
+            remaining = cooldown;
+            health.currentHealth = Math.min(health.getCurrentHealth()+ 2* getDefencePoints(), health.getHealthPool() );
+            return true;
+        }
+    }
 }
