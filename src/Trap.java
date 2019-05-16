589f58b4a586f5927be91c4444ca7f50595133bc
Andrey <palmana@post.bgu.ac.il>
2019-05-16 14:32:42 +0300

    all the basic classes

--- src/Trap.java
+++ src/Trap.java
@@ -1,6 +1,15 @@
 public class Trap extends  Enemy {
 
+    private int realocationRange;
+    private int relocationTime;
+    private int visibilityTime;
+    private int ticksCount;
+
     public Trap(String name, char tile, Health health, int attackPoints, int defencePoints, int experienceValue, int range, int respwan, int visibility ,Position position) {
         super(name, tile, health, attackPoints, defencePoints, experienceValue, position);
+        this.realocationRange = range;
+        this.relocationTime = respwan;
+        this.visibilityTime = visibility;
+        this.ticksCount = 0;
     }
 }
